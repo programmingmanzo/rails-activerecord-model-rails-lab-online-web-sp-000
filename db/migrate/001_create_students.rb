8ceaef5e95111a67d7239b26084aed3752ced39e
Gerardo Manzo <programmingmanzo@gmail.com>
2020-03-24 04:11:05 +0000

    Automatically backed up by Learn

---
+++ db/migrate/001_create_students.rb
@@ -0,0 +1,9 @@
+class CreateStudents < ActiveRecord::Migration[6.0]
+  def change 
+    create_table :students do |t|
+      t.text :first_name 
+      t.text :last_name 
+      
+    end
+  end
+  
